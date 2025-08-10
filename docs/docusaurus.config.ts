5683c384e2fd8675b69cdca5aa003c699f7c3974
Georg Heiler <georg.kf.heiler@gmail.com>
2025-08-10 13:56:04 +0200

    hopefully fix baseurl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,10 +14,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://ascii-supply-networks.github.io',
   baseUrl: '/dagster-slurm',
 
   // GitHub pages deployment config.
