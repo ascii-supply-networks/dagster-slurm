5ed4fe2c26f402317937c5679bd4a733891c09ef
Georg Heiler <georg.kf.heiler@gmail.com>
2025-08-10 13:41:03 +0200

    k

---
+++ projects/dagster-slurm/dagster_slurm_test/dummy_test.py
@@ -0,0 +1,15 @@
+# TODO: remove dummy test
+import pytest
+
+def test_truth():
+    """
+    This is a simple dummy test to ensure the pytest suite runs correctly.
+    It will be discovered by pytest and will always pass.
+    """
+    assert True, "This assertion should always be true."
+
+def test_addition():
+    """
+    A simple test to verify basic arithmetic.
+    """
+    assert 1 + 1 == 2
